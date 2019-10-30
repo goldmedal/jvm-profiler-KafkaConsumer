a0a45953aa844b6976220d1838825ab72ae49e09
goldmedal <liugs963@gmail.com>
2019-10-30 12:01:22 +0800

    fix the name of main class in the start script.

--- sbin/run-consumer.sh
+++ sbin/run-consumer.sh
@@ -12,7 +12,7 @@ else
         --master "local[3]" \
         --conf spark.driver.memory=4g \
         --properties-file  kafka.conf \
-        --class jax.data.profile.KafkaResultConsumer \
+        --class com.wasai.data.profile.KafkaResultConsumer \
         --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 \
         $(pwd)/target/scala-2.11/jvm-profiler-kafkaconsumer_2.11-0.1.jar \
         $PREFIX
